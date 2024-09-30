cc7c532490a860a80cb12f0fa7550c036a138b7e
Mikołaj Gawron <gawrmiko@gmail.com>
2024-09-30 16:48:25 +0200

    feat: change cookies storage
    localStorage -> cookies

--- public/js/cookie-banner.js
+++ public/js/cookie-banner.js
@@ -1,14 +1,31 @@
+function setCookie(name, value, days) {
+  const date = new Date();
+  date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+  const expires = "expires=" + date.toUTCString();
+  document.cookie = `${name}=${value};${expires};path=/`;
+}
+
+function getCookie(name) {
+  const nameEQ = name + "=";
+  const ca = document.cookie.split(";");
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i].trim();
+    if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+}
+
 document.addEventListener("DOMContentLoaded", function () {
   const cookieBanner = document.getElementById("cookie-banner");
   const acceptCookiesButton = document.getElementById("accept-cookies");
 
-  const cookiesAccepted = localStorage.getItem("cookies_accepted");
+  const cookiesAccepted = getCookie("cookies_accepted");
   if (!cookiesAccepted) {
     cookieBanner.classList.remove("d-none");
   }
 
   acceptCookiesButton.addEventListener("click", function () {
-    localStorage.setItem("cookies_accepted", "true");
+    setCookie("cookies_accepted", "true", 365);
     cookieBanner.classList.add("d-none");
   });
 });
